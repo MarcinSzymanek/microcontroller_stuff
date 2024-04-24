91db8561fe00686a4c31b52a1fe4590c0e9f8e3e
Marcin Szymanek <202009418@post.au.dk>
2024-04-24 13:13:15 +0200

    Added adc_ctrl and mux modules to MidiXylo main

---
+++ ESP32/MidiXylo/main/adc_ctrl.hpp
@@ -0,0 +1,21 @@
+#include "esp_log.h"
+#include "esp_adc/adc_oneshot.h"
+#include <vector>
+
+class AdcController{
+public:
+    enum ADC_TYPE{
+        ADC_ONESHOT,
+        ADC_CONTINUOUS
+    };
+
+    AdcController(){}
+    ~AdcController();
+    void init_adc(std::vector<adc_channel_t> channels);
+    int read_adc();
+    int read_adc(int channel);
+private:
+    std::vector<adc_channel_t> channels_;
+    adc_oneshot_unit_handle_t handle_;
+    ADC_TYPE adc_type_ = ADC_TYPE::ADC_ONESHOT;
+};
---
+++ ESP32/MidiXylo/main/mux.hpp
@@ -0,0 +1,29 @@
+#include "stdint.h"
+#include "esp_log.h"
+
+namespace sensors{
+
+typedef struct mux_pin_config_t{
+    gpio_num_t S0;
+    gpio_num_t S1;
+    gpio_num_t S2;
+    gpio_num_t S3;
+} mux_pin_config;
+
+class MuxController{
+    public:
+        MuxController(){}
+        MuxController(mux_pin_config* mux_pins){
+            init_mux(mux_pins);
+        }
+        void init_mux(mux_pin_config* mux_pins);
+        void switch_channel(uint8_t channel);
+    private:
+        mux_pin_config mux_pins_;
+        static int MUX_ID;
+        int mux_id_;
+    };
+
+    int MuxController::MUX_ID = 0;
+} // namespace sensors
+
